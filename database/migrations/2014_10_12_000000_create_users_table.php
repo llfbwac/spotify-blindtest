d93abf990bd409fcd1630a4fb00e64c0d9ac85f7
llfbwac <l.lefollezoubar@webandcow.com>
2023-07-06 21:49:25 +0200

    initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('spotify_id')->unique();
+            $table->string('spotify_name')->nullable();
+            $table->string('spotify_access_token')->nullable();
+            $table->string('spotify_access_token_expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
